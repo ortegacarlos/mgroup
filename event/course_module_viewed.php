5a6965ebd52ae8423ad10659f7fce90e79747357
ortegacarlos <carloslco246@gmail.com>
2019-08-09 20:23:32 -0500

    Modificando el archivo course_module_viewed.php

--- event/course_module_viewed.php
+++ event/course_module_viewed.php
@@ -22,7 +22,7 @@
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-namespace mod_choicegroup\event;
+namespace mod_mpgroup\event;
 
 defined('MOODLE_INTERNAL') || die();
 
