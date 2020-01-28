074dbc2b114d64a852c1c85f75ea18ee700ec1ef
ortegacarlos <carloslco246@gmail.com>
2020-01-28 12:05:17 -0500

    Agregando cadenas de texto en mgroup.php

--- lang/en/mgroup.php
+++ lang/en/mgroup.php
@@ -56,6 +56,7 @@ $string['userfile_help'] = 'Campo para cargar el archivo de texto plano con los 
 $string['enrolled'] = 'Permitir estudiantes no matriculados en el curso';
 $string['enrolled_help'] = 'Seleccione esta opción si desea permitir la formación de grupos con estudiantes no matriculados en su curso.';
 
+$string['groupingparameters'] = 'Parámetros de agrupamiento';
 $string['groupingsettings'] = 'Ajustes de agrupamiento';
 $string['homogeneous'] = 'Homogéneo';
 $string['heterogeneous'] = 'Heterogéneo';
