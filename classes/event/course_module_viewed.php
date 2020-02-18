262a937cd38337d8741adb0399b9878e89472ba5
ortegacarlos <carloslco246@gmail.com>
2020-02-18 15:15:02 -0500

    Archivos de eventos del módulo

--- classes/event/course_module_viewed.php
+++ classes/event/course_module_viewed.php
@@ -35,7 +35,7 @@ class course_module_viewed extends \core\event\course_module_viewed {
      */
     protected function init() {
         $this->data['crud'] = 'r';
-        $this->data['edulevel'] = self::LEVEL_PARTICIPATING;
+        $this->data['edulevel'] = self::LEVEL_TEACHING;
         $this->data['objecttable'] = 'mgroup';
     }
 
