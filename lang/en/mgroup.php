cba418835830e259a88cccf7a6a468d328e9e5ff
ortegacarlos <carloslco246@gmail.com>
2020-03-07 22:23:20 -0500

    Agregando cadenas de texto

--- lang/en/mgroup.php
+++ lang/en/mgroup.php
@@ -51,6 +51,11 @@ $string['selectionoperator_help'] = 'Digite un valor entero correspondiente al p
 $string['mutationoperator'] = 'Operador de mutación';
 $string['mutationoperator_help'] = 'Digite un valor real correspondiente a la probabilidad a considerar para el operador de mutación. El valor por defecto es de 0.2.';
 
+$string['uploadfile'] = 'Subir archivo';
+
+$string['groupingbfi'] = 'Seleccione una opción';
+$string['groupingbfi_help'] = 'Seleccione una opción si desea formar los grupos con los valores obtenidos de la selección del módulo "BFI", de lo contrario seleccione la opción "Subir archivo" para utilizar sus valores y formar los grupos.';
+
 $string['userfile'] = 'Archivo de datos';
 $string['userfile_help'] = 'Campo para cargar el archivo de texto plano con los datos requeridos para la conformación de grupos de trabajo colaborativo.';
 
@@ -84,8 +89,9 @@ $string['err_numberofcharacteristics'] = 'Debe suministrar un valor entero mayor
 $string['err_populationsize'] = 'Debe suministrar un valor entero mayor a 0';
 $string['err_selectionoperator'] = 'Debe suministrar un valor entero entre 1 y 100';
 $string['err_mutationoperator'] = 'Debe suministrar un valor entre 0 y 1';
-$string['err_savefile'] = 'Hubieron errores al guardar el archivo';
-$string['err_readfile'] = 'Hubieron errores al leer el archivo';
+$string['err_savefile'] = 'Error al guardar el archivo o  no se cargó ningún archivo previamente';
+$string['err_createfile'] = 'Error al crear el archivo';
+$string['err_readfile'] = 'Error al leer el archivo';
 $string['err_checkfile'] = 'Revise las inconsistencias encontradas en el archivo';
 $string['err_checkparameters'] = 'Hay inconsistencias en la línea {$a->number}. El número de características ingresadas no coincide con el número de características guardadas en el archivo.';
 $string['err_checkusers'] = 'Revise que los estudiantes se encuentran matriculados en el curso';
